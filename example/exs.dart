93b446a5c5d6333d9325667d0665a0bdab32ac0b
Dzenan Ridjanovic <dzenanr@gmail.com>
2015-01-27 17:49:10 -0500

    add library

---
+++ example/exs.dart
@@ -0,0 +1,7 @@
+import 'package:examples/examples.dart';
+
+main() {
+  print('Two functions');
+  print('area: ${ex3point1(10, 20)} sq m');
+  print('speed: ${ex3point2(100, 9.58)} m/s');
+}
---
+++ lib/examples.dart
@@ -0,0 +1,9 @@
+library examples;
+
+num ex3point1(num width, num height) {
+  return width * height;
+}
+
+num ex3point2(num distance, num time) {
+  return distance / time;
+}
